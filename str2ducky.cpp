aa41151cc41f8b5e42860e312c8f986cd54d5cbb
Sud0nym <49741866+Sud0nym4315@users.noreply.github.com>
2024-08-18 07:47:09 -0400

    Update str2ducky.cpp

--- str2ducky.cpp
+++ str2ducky.cpp
@@ -35,10 +35,13 @@ int main(int argc, char* argv[]) {
     string base64_string((istreambuf_iterator<char>(input_file)), istreambuf_iterator<char>());
     input_file.close();
 
+    //split b64 str into 50 char chunks
     vector<string> chunks = split(base64_string, 50);
 
+    //is it better to append chunks to file via cli as a one-liner or incrementally?
+    //for now, incrementally seems best
     for (const string& chunk : chunks) {
-        output_file << "STRING " << chunk << " >> txt.b64" << endl;
+        output_file << "STRING " << chunk << " >> txt.b64" << endl; //standardize temp file name?
         output_file << "DELAY 350" << endl;
         output_file << "ENTER" << endl;
         output_file << "DELAY 350" << endl;
